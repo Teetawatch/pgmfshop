6994cd1f1d027d4d50d021f5421bdf057a7c50ea
Teetawatch <teetawatch.p@gmail.com>
2026-02-23 20:35:01 +0700

    Fix heroicon component error in contact page
    - Replace non-existent 'heroicon-o-chat-bubble-oval' with 'heroicon-o-chat-bubble-left-right'
    - Contact page now loads without component errors

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -136,7 +136,7 @@
                     </a>
                     <a href="https://line.me" target="_blank" 
                        class="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors">
-                        <x-heroicon-o-chat-bubble-oval class="h-5 w-5 text-gray-600" />
+                        <x-heroicon-o-chat-bubble-left-right class="h-5 w-5 text-gray-600" />
                     </a>
                     <a href="https://twitter.com" target="_blank" 
                        class="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors">
